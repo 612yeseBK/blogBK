761bb21b0eae8c7539e65d4e9d651ace33d89f4e
612yese <240590367@qq.com>
2018-11-24 21:48:19 +0800

    backup commit

---
+++ backup.bat
@@ -0,0 +1,12 @@
+@echo off
+echo "begin to backup"
+xcopy . ..\blogBK /s /e /y /EXCLUDE:.\exclude.ins
+cd ..\blogBK
+echo begin to commit to github
+git add .
+git commit -m "backup commit"
+git push origin master
+cd ..\blog
+echo we have finished the bat
+echo press any key to exit...
+pause>nul
