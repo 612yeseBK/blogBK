99c335578ef0f7a2b853cd54444b75b8afe68ec1
612yese <240590367@qq.com>
2018-11-24 21:10:58 +0800

    backup commit

--- 备份的脚本.bat
+++ 备份的脚本.bat
@@ -1,6 +1,12 @@
 @echo off
 xcopy . ..\blogBK /s /e /y /EXCLUDE:.\exclude.ins
-
+cd ..
+cd blogBK
+echo 开始提交到远程仓库
+pause
+git add .
+git commit -m "backup commit"
+git push origin master
 echo 脚本会自动生成一个备份文件并上传至我的git里面
 echo 已经执行完毕，按任意键退出....
 pause>nul
